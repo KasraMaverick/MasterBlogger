d2a5b5015f6069a78f7e78e8077a66f39a57787a
nadri.k <nadri.k@VDI-DOTNET2-18>
2024-02-27 08:29:49 +0330

    Add project files.

---
+++ ArticleManagement.Domain/ArticleCategoryAgg/ArticleCategory.cs
@@ -0,0 +1,17 @@
+﻿namespace ArticleManagement.Domain.ArticleCategoryAgg
+{
+    public class ArticleCategory
+    {
+        public long Id { get; private set; }
+        public string Title { get;private set; }   
+        public bool IsDeleted { get; private set; }
+        public DateTime CreatedDate { get; private set; }
+
+        public ArticleCategory(string title)
+        {
+            Title = title;
+            IsDeleted = false;
+            CreatedDate = DateTime.Now;
+        }
+    }
+}
